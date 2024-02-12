8ba8f090f151b1e8fd4a373357f54f323b3eda96
VINAYKURREiith <151560682+VINAYKURREiith@users.noreply.github.com>
2024-02-12 20:04:04 +0530

    Add files via upload

--- descrite/assgn2/submit1/codes/6.c
+++ descrite/assgn2/submit1/codes/6.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-#define NUM_POINTS 1000
 
-double max_wavelength(double distance);
+#define MIN_DISTANCE 20    // Minimum distance between towers in km
+#define MAX_DISTANCE 100  // Maximum distance between towers in km
+
+double max_wavelength(double distance) {
+     double z_f = distance*5;
+     double wavelength = (50*50)/z_f;
+     return wavelength;
+}
 
 int main() {
-    double height_of_hill = 50.0;
-    double distances_between_towers[NUM_POINTS];
-    double max_wavelengths[NUM_POINTS];
+    FILE *outputFile = fopen("max_wavelength_data.txt", "w"); // Open file for writing
 
-    // Generate distances between towers from 20 km to 100 km
-    for (int i = 0; i < NUM_POINTS; i++) {
-        distances_between_towers[i] = 20.0 + (80.0 / NUM_POINTS) * i;
-    }
+    if (outputFile) {
+        for (int distance = MIN_DISTANCE; distance <= MAX_DISTANCE; distance++) {
+            double wavelength = max_wavelength(distance);
+            fprintf(outputFile, "%d %lf\n", distance, wavelength);
+        }
 
-    // Calculate max wavelength for each distance
-    for (int i = 0; i < NUM_POINTS; i++) {
-        max_wavelengths[i] = max_wavelength(distances_between_towers[i]);
-    }
-
-    // Print the results
-    printf("Distance (km)\tMax Wavelength (cm)\n");
-    for (int i = 0; i < NUM_POINTS; i++) {
-        printf("%.2f\t\t%.2f\n", distances_between_towers[i], max_wavelengths[i]);
+        fclose(outputFile);
+    } else {
+        printf("Error: Failed to open output file\n");
+        return 1;
     }
 
     return 0;
-}
-
-double max_wavelength(double distance) {
-    double z_f = distance * 5.0;
-    double wavelength = (50.0 * 50.0) / z_f;
-    return wavelength;
 }
 
--- descrite/assgn2/submit1/codes/6.py
+++ descrite/assgn2/submit1/codes/6.py
@@ -1,24 +1,23 @@
-import numpy as np
 import matplotlib.pyplot as plt
-height_of_hill = 50  
-distances_between_towers = np.linspace(20, 100, 1000)  
-def max_wavelength(distance):
-    z_f= distance*5
-    wavelength = (height_of_hill**2)/(z_f)
-    return wavelength
-max_wavelengths = max_wavelength(distances_between_towers)
-plt.figure(figsize=(10, 6))
-plt.plot(distances_between_towers, max_wavelengths, color='blue')
+distances = []
+wavelengths = []
+with open("max_wavelength_data.txt", "r") as file:
+    for line in file:
+        distance, wavelength = map(float, line.split())
+        distances.append(distance)
+        wavelengths.append(wavelength)
+plt.plot(distances, wavelengths)
 plt.xlabel('d(km)')
 plt.ylabel('$\lambda(cm)$')
 plt.grid(True)
 distance_40 = 40
-max_wavelength_40 = max_wavelength(distance_40)
+max_wavelength_40 = wavelengths[20]
 plt.scatter(distance_40, max_wavelength_40, color='red', label='Distance = 40 km')
 plt.annotate(f'Distance = 40 km\nMax: ({distance_40:.2f}, {max_wavelength_40:.2f})',
              xy=(distance_40, max_wavelength_40),
              xytext=(distance_40 + 5, max_wavelength_40 - 0.5),
              arrowprops=dict(facecolor='black', arrowstyle='->'))
 plt.legend()
-plt.savefig('fig/fig2.png')
+plt.savefig('figs/fig2.png')
 plt.show()
+
