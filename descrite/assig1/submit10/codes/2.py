a6cb2ed388c1abd6f5e1931f7f4c82c97afd42f5
VINAYKURREiith <151560682+VINAYKURREiith@users.noreply.github.com>
2024-02-12 19:09:15 +0530

    Update 2.py

--- descrite/assig1/submit10/codes/2.py
+++ descrite/assig1/submit10/codes/2.py
@@ -1,17 +1,4 @@
 import matplotlib.pyplot as plt
-
-def plot_gp(gp_sequence):
-    plt.stem(range(len(gp_sequence)), gp_sequence, markerfmt='r^', linefmt='-', basefmt='b', label='Stem plot')
-    plt.scatter(omega_40, capacitance_40, color='red', label='$\omega$ = 100$\pi$ rads/s\n C = 1.45 mF')
-    plt.title('Geometric Progression')
-    plt.xlabel('Term')
-    plt.ylabel('Value')
-    plt.grid(True)
-    plt.legend()
-    plt.savefig('fig2.png')
-    plt.show()
-
-# Read values from the file
 with open('gp_values.txt', 'r') as file:
     gp_values = [float(line.strip()) for line in file]
     
