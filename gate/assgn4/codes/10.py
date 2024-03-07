e5ca0cebff06795deb5ebe9c446d68fad6fb4db1
VINAYKURREiith <151560682+VINAYKURREiith@users.noreply.github.com>
2024-03-07 15:45:09 +0530

    Add files via upload

--- gate/assgn4/codes/10.py
+++ gate/assgn4/codes/10.py
@@ -1,15 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the function for x(n)
-def x_n(n):
-    return 2*(np.cos(np.pi * n / 3) + 2 * np.cos(2 * np.pi * n / 3) + 3 * np.cos(np.pi * n)) + 2
-
+with open('x_data.txt', 'r') as file:
+    x_values = [float(line.strip()) for line in file]
 # Generate values for n from 0 to 5
 n_values = np.arange(0, 30)
-
-# Compute x(n) for each value of n
-x_values = x_n(n_values)
 
 # Plot the stem plot
 plt.stem(n_values, x_values)
