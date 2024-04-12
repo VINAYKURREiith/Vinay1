8e3eee68604a78405f8a102bef94eb91fc259730
VINAYKURREiith <151560682+VINAYKURREiith@users.noreply.github.com>
2024-04-12 23:50:36 +0530

    Add files via upload

--- filter-design/codes/fig7.py
+++ filter-design/codes/fig7.py
@@ -1,20 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import warnings
 
-# Suppress the warning
-warnings.filterwarnings("ignore", category=RuntimeWarning)
 # Define the piecewise function h_LP(n)
-
+def h_LP(n):
+    if n == 0:
+        return 0.025 / np.pi
+    elif -48 <= n <= 48:
+        return (np.sin(0.025 * n * np.pi)) / (n * np.pi)
+    else:
+        return 0
 
 # Generate values for n
 n_values = np.arange(-480, 491)
-h_Lp = np.where((n_values != 0) & (n_values >= -100) & (n_values <= 100), np.sin(n_values * np.pi / 40) / (n_values * np.pi), 0)
 
-
+# Calculate h_BP values for each n
+h_values = np.array([h_LP(n) for n in n_values])
 
 # Calculate the DTFT using numpy's FFT
-H_freq_response = np.fft.fftshift(np.fft.fft(h_Lp))
+H_freq_response = np.fft.fftshift(np.fft.fft(h_values))
 
 # Angular frequency axis (normalized)
 omega_normalized = np.linspace(-np.pi/2, np.pi/2, len(n_values))
